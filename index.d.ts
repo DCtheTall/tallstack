752d265abc94e64a2f7686382ed39bfcb46e91b3
Dylan Cutler <dylancutler@google.com>
2019-03-06 19:47:19 -0500

    fix mistake

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-export function call<S extends any[], T>(func: (...args: S) => T): T;
+export function call<S extends any[], T>(func: (...args: S) => T, ...args: S): T;
 
 export function callWithContext<S extends any[], T>(
   thisArg: any,
